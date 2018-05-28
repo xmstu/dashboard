afcbe915680a7ea15bb7367543de141b1800c7fa
master <xuex@huitouche.com>
2018-05-28 18:53:11 +0800

    [add]用户统计接口

--- server/route/user.py
+++ server/route/user.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+
+from server import app
+from flask import render_template, session
+
+@app.route('/user/')
+def user():
+    """用户统计页面"""
+    if not session.get('login'):
+        return render_template('/login/login.html')
+    return render_template('/user/user-statistics.html')
