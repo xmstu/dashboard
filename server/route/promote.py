6c04ca74bcd3102c2aa3f529257f1abe158698ed
master <xuex@huitouche.com>
2018-05-29 13:59:24 +0800

    路由配置

--- server/route/promote.py
+++ server/route/promote.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+
+from server import app
+from flask import render_template, session
+
+@app.route('/promote/')
+def promote():
+    """推广统计页面"""
+    if not session.get('login'):
+        return render_template('/login/login.html')
+    return render_template('/promote/promote-statistics.html')
