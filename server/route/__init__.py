e188f56cd2151440be7e363ba099f3bf26b207be
wangpengfei <wangpf@huitouche.com>
2018-06-01 12:15:32 +0800

    router-set

--- server/route/__init__.py
+++ server/route/__init__.py
@@ -4,3 +4,6 @@ from server.route.login import *
 from server.route.user import *
 from server.route.goods import *
 from server.route.promote import *
+from server.route.admin import *
+from server.route.password import *
+from server.route.transport import *
