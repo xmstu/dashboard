a5e1754a7c12c46361c321c922244de3ce6a7d93
master <xuex@huitouche.com>
2018-06-24 16:58:30 +0800

    优化用户统计查询速度

--- server/models/user.py
+++ server/models/user.py
@@ -56,10 +56,12 @@ class UserList(object):
             LIMIT 1) AS usual_city,
             -- 发货数
             (SELECT COUNT(*) FROM shf_goods WHERE shf_goods.user_id = shu_users.id) AS goods_count,
-            -- 订单数
-            (SELECT COUNT(*) FROM shb_orders WHERE shb_orders.owner_id = shu_users.id OR shb_orders.driver_id = shu_users.id) AS order_count,
+           -- 订单数
+            (SELECT COUNT(*) FROM shb_orders WHERE shb_orders.owner_id = shu_users.id) +
+            (SELECT COUNT(*) FROM shb_orders WHERE shb_orders.driver_id = shu_users.id) AS order_count,
             -- 订单完成
-            (SELECT COUNT(*) FROM shb_orders WHERE (shb_orders.owner_id = shu_users.id OR shb_orders.driver_id = shu_users.id) AND shb_orders.`status` = 3 AND (shb_orders.pay_status = 2 OR shb_orders.paid_offline = 1)) AS order_completed,
+            (SELECT COUNT(*) FROM shb_orders WHERE shb_orders.owner_id = shu_users.id AND shb_orders.`status` = 3 AND (shb_orders.pay_status = 2 OR shb_orders.paid_offline = 1)) +
+            (SELECT COUNT(*) FROM shb_orders WHERE shb_orders.driver_id = shu_users.id AND shb_orders.`status` = 3 AND (shb_orders.pay_status = 2 OR shb_orders.paid_offline = 1)) AS order_completed,
             -- 下载、注册渠道
             shu_user_profiles.download_channel,
             shu_user_profiles.from_channel,
@@ -71,8 +73,8 @@ class UserList(object):
             SELECT
             %s
             FROM shu_users
-            LEFT JOIN shu_user_profiles ON shu_users.id = shu_user_profiles.user_id
-            LEFT JOIN shu_user_stats ON shu_users.id = shu_user_stats.user_id
+            INNER JOIN shu_user_profiles ON shu_users.id = shu_user_profiles.user_id
+            INNER JOIN shu_user_stats ON shu_users.id = shu_user_stats.user_id
             -- 被推荐
             LEFT JOIN shu_recommended_users AS recommended_user ON shu_users.id = recommended_user.recommended_user_id
 
