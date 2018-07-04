745c6f17995559ddf32f4faa606cec3361953b8d
master <xuex@huitouche.com>
2018-07-04 13:57:09 +0800

    后台用户地区权限全开

--- server/route/transport.py
+++ server/route/transport.py
@@ -15,4 +15,5 @@ def transport():
     avatar_url = session['login'].get('avatar_url', 'https://mp.huitouche.com/static/images/newicon.png')
     locations = [{'region_id': i, 'name': init_regions.to_full_short_name(i)} for i in
                  session['login'].get('locations', [])]
-    return render_template('/transport/transport-capacity.html', user_name=user_name, avatar_url=avatar_url, locations=locations)
+    role = session['login'].get('role', 0)
+    return render_template('/transport/transport-capacity.html', user_name=user_name, avatar_url=avatar_url, locations=locations, role=role)
