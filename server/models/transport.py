3183c01fc4de3c1ad317bb85e71e38aa86e0e9ad
leo_he <hexm@huitouche.com>
2018-07-04 16:28:20 +0800

    运力统计接口更新

--- server/models/transport.py
+++ server/models/transport.py
@@ -7,6 +7,7 @@ class TransportRadarModel(object):
 
         goods_cmd = """
         SELECT
+            shf_goods_vehicles.name,
             COUNT( 1 ) goods_count
         FROM
             shf_goods
@@ -17,25 +18,33 @@ class TransportRadarModel(object):
             AND shf_goods.create_time < :end_time
         WHERE
             {goods_sql}
+            AND shf_goods_vehicles.name IN ('小面包车', '中面包车', '小货车', '4.2米', '6.8米', '7.6米', '9.6米', '13米', '17米', '17.5米')
+            GROUP BY shf_goods_vehicles.NAME
         """
 
         vehicle_cmd = """
         SELECT
+            shm_dictionary_items.name,
             COUNT( 1 ) vehicle_count
         FROM
-            shf_booking_settings -- 在时间段内有登录过的用户
-            INNER JOIN shu_user_stats ON shf_booking_settings.user_id = shu_user_stats.user_id 
+            shu_vehicle_auths
+            INNER JOIN shu_vehicles ON shu_vehicle_auths.vehicle_id = shu_vehicles.id 
+            INNER JOIN shf_booking_settings ON shf_booking_settings.user_id = shu_vehicles.user_id
             INNER JOIN shf_booking_basis ON shf_booking_settings.user_id = shf_booking_basis.user_id
-            INNER JOIN shm_dictionary_items ON shf_booking_basis.short_haul_vehicle_length_id = shm_dictionary_items.id
-            AND shu_user_stats.last_login_time >= :start_time 
-            AND shu_user_stats.last_login_time < :end_time  
-            AND shf_booking_settings.is_deleted = 0 
+            INNER JOIN shm_dictionary_items ON shm_dictionary_items.id = shu_vehicle_auths.length_id 
+            -- 	接单线路创建时间
+            AND shf_booking_settings.create_time >=:start_time
+            AND shf_booking_settings.create_time < :end_time
+            AND shu_vehicle_auths.is_deleted = 0 AND shu_vehicle_auths.auth_status = 2 
         WHERE
             {vehicle_sql}
+            AND shm_dictionary_items.`name` IN ('小面包车', '中面包车', '小货车', '4.2米', '6.8米', '7.6米', '9.6米', '13米', '17米', '17.5米')
+            GROUP BY shm_dictionary_items.`name`
         """
 
         order_cmd = """
         SELECT
+            shf_goods_vehicles.name,
             COUNT( 1 ) order_count
         FROM
             shb_orders INNER JOIN shf_goods ON shf_goods.id = shb_orders.goods_id
@@ -46,6 +55,8 @@ class TransportRadarModel(object):
             AND shb_orders.create_time < :end_time
         WHERE
             {order_sql}
+            AND shf_goods_vehicles.name IN ('小面包车', '中面包车', '小货车', '4.2米', '6.8米', '7.6米', '9.6米', '13米', '17米', '17.5米')
+            GROUP BY shf_goods_vehicles.name
         """
 
         goods_sql = """ 1=1 """
@@ -152,25 +163,34 @@ class TransportRadarModel(object):
             'end_time': params.get('end_time')
         }
 
-        vehicle_name_list = ['小面包车', '中面包车', '小货车', '4.2米', '6.8米', '7.6米', '9.6米', '13米', '17米']
+        vehicle_name_list = ['小面包车', '中面包车', '小货车', '4.2米', '6.8米', '7.6米', '9.6米', '13米', '17米', '17.5米']
 
-        # 车型
+        goods_count = cursor.query(goods_cmd.format(goods_sql=goods_sql), kwargs)
+        vehicle_count = cursor.query(vehicle_cmd.format(vehicle_sql=vehicle_sql), kwargs)
+        order_count = cursor.query(order_cmd.format(order_sql=order_sql), kwargs)
+
+        goods_dict = {}
+        vehicles_dict = {}
+        orders_dict ={}
+
+        for i in goods_count:
+            goods_dict[i.get('name')] = i.get('goods_count')
+
+        for i in vehicle_count:
+            vehicles_dict[i.get('name')] = i.get('vehicle_count')
+
+        for i in order_count:
+            orders_dict[i.get('name')] = i.get('order_count')
+
         goods_ret = []
         vehicles_ret = []
         orders_ret = []
 
-        goods_sql += """ AND shf_goods_vehicles.NAME = '%s' """
-        vehicle_sql += """ AND shm_dictionary_items.`name` = '%s' """
-        order_sql += """ AND shf_goods_vehicles.NAME = '%s' """
-
+        # 车型
         for i in vehicle_name_list:
-            goods_count = cursor.query_one(goods_cmd.format(goods_sql=goods_sql % i), kwargs)['goods_count']
-            vehicle_count = cursor.query_one(vehicle_cmd.format(vehicle_sql=vehicle_sql % i), kwargs)['vehicle_count']
-            order_count = cursor.query_one(order_cmd.format(order_sql=order_sql % i), kwargs)['order_count']
-
-            goods_ret.append(goods_count if goods_count else 0)
-            vehicles_ret.append(vehicle_count if vehicle_count else 0)
-            orders_ret.append(order_count if order_count else 0)
+            goods_ret.append(goods_dict.get(i, 0))
+            vehicles_ret.append(vehicles_dict.get(i, 0))
+            orders_ret.append(orders_dict.get(i, 0))
 
         data = {
             'vehicle_name_list': vehicle_name_list,
