8cb90c8d3f21c76b40b26b0c2ac61bc931eecbfe
leo_he <hexm@huitouche.com>
2018-07-06 17:19:58 +0800

    x

--- server/models/heat_map.py
+++ server/models/heat_map.py
@@ -1,10 +1,11 @@
+import time
+
+
 class HeatMapModel(object):
 
     @staticmethod
     def get_user(cursor, params):
         fields = """"""
-
-        which_table = """"""
 
         fetch_where = """ 1=1 """
 
@@ -16,21 +17,8 @@ class HeatMapModel(object):
             `tb_inf_user` 
         WHERE
             {fetch_where} 
-        -- 注册角色是全部/司机/货主/物流公司
-        -- 	AND user_type = 3
-        -- 认证角色是司机
-        -- 	AND driver_auth = 1
-        -- 认证角色是货主
-        -- 	AND goods_auth = 1
-        -- 认证角色是物流公司
-        -- 	AND company_auth = 1
-        -- 	全部已认证角色
-        -- 	AND ( driver_auth = 1 OR goods_auth = 1 OR company_auth = 1 ) 
-        -- 活跃数
-        -- 	AND keep_login_days >= 7 AND last_login_time > UNIX_TIMESTAMP() - 1 * 86400
-            
-            AND create_time >= 1395244800 
-            AND create_time < 1530633600
+            AND create_time >= :start_time 
+            AND create_time < :end_time
             AND from_province_id = 440000 AND from_city_id != 0
             GROUP BY
                 from_city_id
@@ -39,24 +27,65 @@ class HeatMapModel(object):
         region = ' AND 1=1 '
         if params['region_id']:
             if isinstance(params['region_id'], int):
-                region = 'AND (shb_orders.from_province_id = %(region_id)s OR shb_orders.from_city_id = %(region_id)s OR shb_orders.from_county_id = %(region_id)s OR shb_orders.from_town_id = %(region_id)s) ' % {
+                region = 'AND (tb_inf_user.from_province_id = %(region_id)s OR tb_inf_user.from_city_id = %(region_id)s OR tb_inf_user.from_county_id = %(region_id)s OR tb_inf_user.from_town_id = %(region_id)s) ' % {
                     'region_id': params['region_id']}
             elif isinstance(params['region_id'], list):
                 region = '''
-                                        AND (
-                                        shb_orders.from_province_id IN (%(region_id)s)
-                                        OR shb_orders.from_city_id IN (%(region_id)s)
-                                        OR shb_orders.from_county_id IN (%(region_id)s)
-                                        OR shb_orders.from_town_id IN (%(region_id)s)
-                                        ) ''' % {'region_id': ','.join(params['region_id'])}
+                        AND (
+                        tb_inf_user.from_province_id IN (%(region_id)s)
+                        OR tb_inf_user.from_city_id IN (%(region_id)s)
+                        OR tb_inf_user.from_county_id IN (%(region_id)s)
+                        OR tb_inf_user.from_town_id IN (%(region_id)s)
+                        ) ''' % {'region_id': ','.join(params['region_id'])}
 
         fetch_where += region
 
         # 角色
         if params.get('filter'):
-            fetch_where += """"""
+            fetch_where += """
+            AND 
+            (
+            ( {filter}=1 AND user_type = 1) OR
+            ( {filter}=2 AND user_type = 2) OR
+            ( {filter}=3 AND user_type = 3) 
+            )
+            """.format(filter=params['filter'])
 
-        data = cursor.query(command)
+        # 字段
+        if params.get('field'):
+            # 求用户总数
+            if params['field'] == 1:
+                pass
+            # 求角色对应的认证数
+            elif params['field'] == 2:
+                if params.get('filter') == 1:
+                    fetch_where += """
+                    AND driver_auth = 1
+                    """
+                elif params.get('filter') == 2:
+                    fetch_where += """
+                    AND goods_auth = 1
+                    """
+                elif params.get('filter') == 3:
+                    fetch_where += """
+                    AND company_auth = 1
+                    """
+                else:
+                    fetch_where += """
+                    AND ( driver_auth = 1 OR goods_auth = 1 OR company_auth = 1 ) 
+                    """
+            # 求活跃数
+            elif params['field'] == 3:
+                fetch_where += """
+                AND keep_login_days >= 7 AND last_login_time > UNIX_TIMESTAMP() - 86400
+                """
+
+        kwargs = {
+            "start_time": params.get('start_time', time.time() - 86400*7),
+            "end_time": params.get('end_time', time.time() - 86400)
+        }
+
+        data = cursor.query(command, kwargs)
 
         return data
 
