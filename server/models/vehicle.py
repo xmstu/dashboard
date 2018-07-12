904a6d8815948ccb0f22c97083668b78a7749e57
master <xuex@huitouche.com>
2018-07-12 21:29:37 +0800

    修复获取车长错误

--- server/models/vehicle.py
+++ server/models/vehicle.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from server.logger import log
+
 class VehicleModel(object):
     @staticmethod
     def get_vehicle_length_name(cursor, length_id):
         """获取车长名称"""
-        command = '''
-        SELECT `name`
-        FROM shm_dictionary_items
-        WHERE id = :length_id'''
+        try:
+            command = '''
+            SELECT `name`
+            FROM shm_dictionary_items
+            WHERE id = :length_id'''
 
-        result = cursor.query_one(command)
-        return result['name'] if result else ''
+            result = cursor.query_one(command, {
+                'length_id': length_id
+            })
+
+            return result['name'] if result else ''
+        except Exception as e:
+            log.error('获取车长名称出错: [length_id: %s][e: %s]' % (length_id, e), exc_info=True)
