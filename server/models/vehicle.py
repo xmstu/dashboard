3b9a79960858f4e0026446c7f897b0a816aaeb55
master <xuex@huitouche.com>
2018-07-16 17:06:11 +0800

    kafka manager监控

--- server/models/vehicle.py
+++ server/models/vehicle.py
@@ -4,18 +4,33 @@ from server.logger import log
 
 class VehicleModel(object):
     @staticmethod
-    def get_vehicle_length_name(cursor, length_id):
+    def get_vehicle_length_name(cursor, length_id, user_id):
         """获取车长名称"""
         try:
             command = '''
-            SELECT `name`
-            FROM shm_dictionary_items
-            WHERE id = :length_id'''
+            SELECT
+            (SELECT COUNT(1) FROM shb_orders WHERE driver_id = shu_users.id) AS order_count,
+            (SELECT COUNT(1) FROM shb_orders WHERE driver_id = shu_users.id AND shb_orders.`status` = 3) AS order_finished,
+            (SELECT COUNT(1) FROM shb_orders WHERE driver_id = shu_users.id AND shb_orders.`status` = -1) AS order_cancel,
+            (SELECT `name` FROM shm_dictionary_items WHERE id = :length_id),
+            (SELECT `value` FROM shm_dictionary_items WHERE id = :length_id)
+            FROM shu_users
+            WHERE id = :user_id '''
 
             result = cursor.query_one(command, {
+                'user_id': user_id,
                 'length_id': length_id
             })
 
-            return result['name'] if result else ''
+            return result if result else ''
         except Exception as e:
             log.error('获取车长名称出错: [length_id: %s][e: %s]' % (length_id, e), exc_info=True)
+
+    @staticmethod
+    def get_user_order_vehicle_data(cursor, user_id, length_id):
+        """获取用户订单和车型数据"""
+        try:
+            command = ''''''
+
+        except Exception as e:
+            log.error('获取用户订单和车型数据出错: [user_id : %s][length_id: %s][e : %s]' % (user_id, length_id, e), exc_info=True)
--- server/operations/city.py
+++ server/operations/city.py
@@ -5,6 +5,8 @@ from server.meta.decorators import make_decorator, Response
 from server.models.city import CityOrderListModel, CityResourceBalanceModel, CityNearbyCarsModel
 from server.models.vehicle import VehicleModel
 from server.database import pyredis
+from server.utils.amap import distance
+
 import time
 
 class CityResourceBalance(object):
@@ -47,22 +49,25 @@ class CityNearbyCars(object):
                 driver = []
                 for i in all_drivers:
                     result = pyredis.token.read_one('online:position:%s' % i['user_id'])
-                    if result and result['province'] == goods['from_province_id'] \
-                    and result['city'] == goods['from_city_id'] \
-                    and result['county'] == goods['from_county_id'] \
-                    and int(time.time() - time.mktime(time.strptime(result['location_time'], '%Y-%m-%d %H:%M:%S'))) < 86400:
-                        # 车长
+                    last_delta = int(time.time() - time.mktime(time.strptime(result['location_time'], '%Y-%m-%d %H:%M:%S')))
+                    if result and result['county'] == goods['from_county_id'] and last_delta < 86400:
+                        # 订单、车长数据
                         length_id = str(i['vehicle_length_id']).split(',')[0]
-                        i['vehicle_length_id'] = VehicleModel.get_vehicle_length_name(db.read_db, int(length_id))
+                        user_info = VehicleModel.get_vehicle_length_name(db.read_db, int(length_id), i['user_id'])
+                        if int(goods['inner_length']) > int(user_info['value']):
+                            continue
                         i.update({
                             'address': result['address'],
                             'longitude': result['longitude'],
                             'latitude': result['latitude'],
                             'last_login_time': result['location_time'],
-                            'last_delta': int(time.time() - time.mktime(time.strptime(result['location_time'], '%Y-%m-%d %H:%M:%S'))),
+                            'last_delta': last_delta,
                             'province': result['province'],
                             'city': result['city'],
-                            'county': result['county']
+                            'county': result['county'],
+                            'order_count': user_info['order_count'],
+                            'order_finished': user_info['order_finished'],
+                            'order_cancel': user_info['order_cancel']
                         })
                         driver.append(i)
                     if len(driver) >= 10:
