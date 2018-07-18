17b88a5becdb42adbeb2ed2f0da124f0500a4097
leo_he <hexm@huitouche.com>
2018-07-18 10:27:56 +0800

    x

--- server/document/verify_vehicle.py
+++ server/document/verify_vehicle.py
@@ -1,19 +1,15 @@
 from server import api
 
+
 verify_vehicle_list_param = api.doc(params={
-    "from_province_id": "出发地省份id",
-    "from_city_id": "出发地城市id",
-    "from_county_id": "出发地区县id",
-    "from_town_id": "出发地城镇id",
-    "to_province_id": "目的地省份id",
-    "to_city_id": "目的地城市id",
-    "to_county_id": "目的地区县id",
-    "to_town_id": "目的地城镇id",
+    "mobile": "手机号",
+    "vehicle_number": "车牌号码",
+    "residence": "常驻地",
     "vehicle_length": "车长要求",
-    "business": "业务类型:1.同城;2.跨城",
-    "filter": "筛选条件:1.货源>车辆;2.货源<车辆;",
-    "start_time": "开始时间",
-    "end_time": "结束时间",
+    "verify_start_time": "认证开始时间",
+    "verify_end_time": "认证结束时间",
+    "last_login_start_time": "最后登录开始时间",
+    "last_login_end_time": "最后登录结束时间",
     "page": "页数",
     "limit": "条数",
 }, description='运力列表查询参数')
