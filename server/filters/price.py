c87b0b0bac90358cf61313043e87f875dc1508d8
leo_he <hexm@huitouche.com>
2018-07-20 17:18:22 +0800

    价格统计接口更新

--- server/filters/price.py
+++ server/filters/price.py
@@ -1,6 +1,8 @@
+import datetime
+import time
+
 from server.meta.decorators import make_decorator
 from server.status import make_result, APIStatus, HTTPStatus
-from server.utils.extend import get_struct_data
 
 
 class PriceTrend(object):
@@ -11,15 +13,27 @@ class PriceTrend(object):
         price_trend = data.get('price_trend', [])
         # 按日
         # xAxis = []
-        price_trend_series = []
+        # price_trend_series = []
         avg_price = []
+        date_count = {}
+        date_val = begin_date = datetime.datetime.strptime(time.strftime("%Y-%m-%d", time.localtime(params['start_time'])), "%Y-%m-%d")
+        end_date = datetime.datetime.strptime(time.strftime("%Y-%m-%d", time.localtime(params['end_time'])), "%Y-%m-%d")
         for detail in price_trend:
             if detail.get('create_time'):
                 create_time = detail['create_time']
                 # xAxis.append(create_time)
                 trend_series = [detail.get('max_price', 0), detail.get('min_price', 0)] * 2
-                price_trend_series.append([create_time] + trend_series)
+                date_count[create_time] = [create_time] + trend_series
+                # price_trend_series.append([create_time] + trend_series)
                 avg_price.append(detail.get('avg_price', 0))
+
+        while date_val <= end_date:
+            pass
+            date_str = date_val.strftime("%Y-%m-%d")
+            date_count.setdefault(date_str, [date_str, 0, 0, 0, 0])
+            date_val += datetime.timedelta(days=1)
+
+        price_trend_series = list(date_count.values())
 
         ret = {
             # "xAxis": xAxis,
