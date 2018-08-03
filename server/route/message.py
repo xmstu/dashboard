0fd13fe01f68b82d6a38a6171e0d05bce635527a
master <xuex@huitouche.com>
2018-08-03 16:00:07 +0800

    消息管理路由

--- server/route/message.py
+++ server/route/message.py
@@ -10,7 +10,7 @@ from server.meta.login_record import visitor_record
 @app.route('/message/', endpoint='message')
 @visitor_record
 def message():
-    """消息中心"""
+    """消息列表"""
     if not sessionOperationClass.check():
         return redirect('/login/')
     # 用户名，头像, 地区
@@ -24,4 +24,19 @@ def message():
         locations = init_regions.get_city_next_region(session['login'].get('locations', []))
     return render_template('/message/message.html', user_name=user_name, avatar_url=avatar_url, locations=locations, role=role, account=account)
 
-# app.add_url_rule('/order/', 'order', order)
+@app.route('/edit-message/', endpoint='edit-message')
+@visitor_record
+def message():
+    """消息管理"""
+    if not sessionOperationClass.check():
+        return redirect('/login/')
+    # 用户名，头像, 地区
+    user_name = session['login'].get('user_name', '')
+    account = session['login'].get('account', '')
+    avatar_url = session['login'].get('avatar_url', 'https://mp.huitouche.com/static/images/newicon.png')
+    locations = [{'region_id': i, 'name': init_regions.to_full_short_name(i)} for i in
+                 session['login'].get('locations', [])]
+    role = session['login'].get('role', 0)
+    if role == 4:
+        locations = init_regions.get_city_next_region(session['login'].get('locations', []))
+    return render_template('/message/edit-message.html', user_name=user_name, avatar_url=avatar_url, locations=locations, role=role, account=account)
