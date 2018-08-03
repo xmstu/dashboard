5a78fdfd9fbc79dfaaca56f624ba9fc25bf12a2e
master <xuex@huitouche.com>
2018-08-03 11:44:12 +0800

    用户消息列表-时间格式化

--- server/filters/message.py
+++ server/filters/message.py
@@ -20,7 +20,16 @@ class MessageUser(object):
         un_read = []
         # 时间格式化
         for i in data:
-            i['create_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(i['create_time']))
+            # 时间间隔
+            last_delta = int(time.time() - i['create_time'])
+            delta = ''
+            if last_delta // 86400 > 0:
+                delta = '%d天前' % (last_delta // 86400)
+            elif last_delta // 3600 > 0:
+                delta = '%d小时前' % (last_delta // 3600)
+            elif last_delta // 60 >= 0:
+                delta = '%d分钟前' % (last_delta // 60)
+            i['create_time'] = delta
             if i['is_read'] == 0:
                 un_read.append(i)
             else:
