443b5116c4db32d905332ce71af4ec359e85bb57
master <xuex@huitouche.com>
2018-08-03 11:06:29 +0800

    添加account属性

--- server/route/message.py
+++ server/route/message.py
@@ -15,12 +15,13 @@ def message():
         return redirect('/login/')
     # 用户名，头像, 地区
     user_name = session['login'].get('user_name', '')
+    account = session['login'].get('account', '')
     avatar_url = session['login'].get('avatar_url', 'https://mp.huitouche.com/static/images/newicon.png')
     locations = [{'region_id': i, 'name': init_regions.to_full_short_name(i)} for i in
                  session['login'].get('locations', [])]
     role = session['login'].get('role', 0)
     if role == 4:
         locations = init_regions.get_city_next_region(session['login'].get('locations', []))
-    return render_template('/message/message.html', user_name=user_name, avatar_url=avatar_url, locations=locations, role=role)
+    return render_template('/message/message.html', user_name=user_name, avatar_url=avatar_url, locations=locations, role=role, account=account)
 
 # app.add_url_rule('/order/', 'order', order)
