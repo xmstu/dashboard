c483f88f376fe5e849a98d84f51f44656bddbfe2
leo_he <hexm@huitouche.com>
2018-08-06 19:23:45 +0800

    用户管理页面接口更新

--- server/resources/root.py
+++ server/resources/root.py
@@ -8,6 +8,7 @@ import server.document.root as doc
 from server import api
 from server import verify, operations, filters
 from server.meta.decorators import Response
+from server.meta.session_operation import sessionOperationClass
 from server.utils.request import *
 
 
@@ -18,24 +19,41 @@ class RootManagement(Resource):
     @operations.RootManagement.get_data(params=dict)
     @verify.RootManagement.check_get_params(params=dict)
     def get():
-        """超级用户管理列表"""
-        resp = Response(params=get_all_arg())
-
-        return resp
+        """获取城市经理或合伙人管理列表"""
+        if sessionOperationClass.check():
+            role, _ = sessionOperationClass.get_role()
+            if role == 1:
+                resp = Response(params=get_all_arg())
+                return resp
+            abort(HTTPStatus.BadRequest, **make_result(status=APIStatus.BadRequest, msg='仅限后台用户获取消息列表'))
+        abort(HTTPStatus.BadRequest, **make_result(status=APIStatus.BadRequest, msg='未登录用户'))
 
     @staticmethod
     @doc.request_root_management_add
-    @operations.RootManagement.add_data(params=dict)
-    @verify.RootManagement.check_add_params(params=dict)
-    def add():
+    @operations.RootManagement.post_data(params=dict)
+    @verify.RootManagement.check_post_params(params=dict)
+    def post():
         """增加新的城市经理或合伙人"""
-        pass
+        if sessionOperationClass.check():
+            role, _ = sessionOperationClass.get_role()
+            if role == 1:
+                resp = Response(params=get_all_arg())
+                return resp
+            abort(HTTPStatus.BadRequest, **make_result(status=APIStatus.BadRequest, msg='仅限后台用户获取消息列表'))
+        abort(HTTPStatus.BadRequest, **make_result(status=APIStatus.BadRequest, msg='未登录用户'))
 
     @staticmethod
     @operations.RootManagement.delete_data(params=dict)
     def delete(id):
         """删除该账户"""
-        pass
+        if sessionOperationClass.check():
+            role, _ = sessionOperationClass.get_role()
+            if role == 1:
+                params = get_all_arg()
+                params['user_id'] = id
+                return Response(params=params)
+            abort(HTTPStatus.BadRequest, **make_result(status=APIStatus.BadRequest, msg='仅限后台用户获取消息列表'))
+        abort(HTTPStatus.BadRequest, **make_result(status=APIStatus.BadRequest, msg='未登录用户'))
 
     @staticmethod
     @doc.request_root_management_put
@@ -43,7 +61,14 @@ class RootManagement(Resource):
     @verify.RootManagement.check_put_params(params=dict)
     def put(id):
         """修改当前用户id的账号或者密码"""
-        pass
+        if sessionOperationClass.check():
+            role, _ = sessionOperationClass.get_role()
+            if role == 1:
+                params = get_all_arg()
+                params['user_id'] = id
+                return Response(params=params)
+            abort(HTTPStatus.BadRequest, **make_result(status=APIStatus.BadRequest, msg='仅限后台用户获取消息列表'))
+        abort(HTTPStatus.BadRequest, **make_result(status=APIStatus.BadRequest, msg='未登录用户'))
 
 
 ns = api.namespace('root', description='超级用户管理')
