86fdd97f10a1e44d3167ab729bb6cd8844d25464
wangpengfei <wangpf@huitouche.com>
2018-08-09 18:26:22 +0800

    root

--- server/static/js/message/message.js
+++ server/static/js/message/message.js
@@ -20,24 +20,28 @@ var set = {
                 var data = res.data;
                 var str = '';
                 for (var i = 0; i < count; i++) {
-                    str += '<div class="layui-colla-item">';
-                    str += '<div data-value="'+data[i].id+'" class="layui-colla-title">' + data[i].title;
-                    str += '<p class="layui-colla-title-child"><span>' + data[i].date + '</span><i class="read-status orange">' + _that.setAbout(data[i].is_read) + '</i></p></div>';
-                    str += '<div class="layui-colla-content">' + data[i].content + '</div></div>';
+                     str += '<div class="layui-colla-item">';
+                        str += '<div data-value="' + data[i].id + '" class="layui-colla-title">' + data[i].title;
+                        str += '<p class="layui-colla-title-child"><span>' + data[i].date + '</span><i class="read-status orange">' + _that.setAbout(data[i].is_read) + '</i></p></div>';
+                    if (i == 0) {
+                        str += '<div class="layui-colla-content  layui-show">' + data[i].content + '</div></div>';
+                    } else {
+                        str += '<div class="layui-colla-content ">' + data[i].content + '</div></div>';
+                    }
                 }
                 $('.layui-collapse').html(_that.resetData(str));
                 element.init();//告诉layui对js模板重新渲染
                 element.on('collapse(message_list)', function (data) {
-                    if(data.show==true){
-                        var url = '/message/user/'+$(this).attr('data-value')+'/?account='+$('#user-info').attr('data-account');
-                        var data  = {};
-                        var _that  = $(this);
-                        http.ajax.get_no_loading(true,false,url,data,http.ajax.CONTENT_TYPE_2,function(){
-                            if(_that.find('.read-status').text()=='未读'&&res.status==100000){
-                              _that.find('.read-status').html('已读')
+                    if (data.show == true) {
+                        var url = '/message/user/' + $(this).attr('data-value') + '/?account=' + $('#user-info').attr('data-account');
+                        var data = {};
+                        var _that = $(this);
+                        http.ajax.get_no_loading(true, false, url, data, http.ajax.CONTENT_TYPE_2, function () {
+                            if (_that.find('.read-status').text() == '未读' && res.status == 100000) {
+                                _that.find('.read-status').html('已读')
                             }
                         })
-                     }
+                    }
                 });
                 laypage.render({
                     elem: $('#pagination')
@@ -59,10 +63,10 @@ var set = {
             return '未读'
         }
     },
-    resetData:function(str){
+    resetData: function (str) {
         /*js反转义*/
-        str = str.replace(/&lt;/g,'<');
-        str = str.replace(/&gt;/g,'>');
+        str = str.replace(/&lt;/g, '<');
+        str = str.replace(/&gt;/g, '>');
         return str
     }
 };
---
+++ server/static/js/root/root.js
@@ -0,0 +1,115 @@
+var set = {
+    init: function () {
+        var secondMenu = document.getElementById('second_menu_list');
+        secondMenu.style.display = 'block';
+        $('#second_menu_list>li:nth-of-type(2) a').addClass('selected-active');
+        $('#second_menu_box').addClass('menu-active')
+        layui.use(['layer', 'table'], function () {
+            var layer = layui.layer;
+            var table = layui.table;
+            var url = '/root/management/'
+           var tableIns = table.render({
+                elem: '#root_table',
+                even: true,
+                url: url,
+                skin: 'nob',
+                response: {
+                    statusName: 'status',
+                    statusCode: 100000
+                }
+                , cols: [[
+                    {field: 'user_name', title: '姓名'},
+                    {field: 'account', title: '手机号'},
+                    {field: 'region_name', title: '所属城市'},
+                    {
+                        field: 'region_id', title: '操作', width: 120, templet: function (d) {
+                            return '<i class="layui-icon layui-icon-edit edit-icon" title="编辑"  style="margin-right: 40px;cursor:pointer;">&#xe642;</i><i data-id="' + d.id + '" style="cursor: pointer" class="layui-icon delete-icon"  title="删除" >&#xe640;</i>'
+                        }
+                    }
+                ]],
+                done: function (res) {
+                    layer.closeAll('loading');
+                    $('.edit-icon').click(function () {
+                        var content = $(this).parents('tr').children('td:eq(0)').find('.layui-table-cell').text();
+                        var location = $(this).parents('tr').children('td:eq(2)').find('.layui-table-cell').text();
+                        $('#name_edit').val(content);
+                        $('#city_picker_search_second').val(location);
+                        layer.open({
+                            type: 1,
+                            title: '编辑信息',
+                            closeBtn: 1,
+                            shadeClose: true,
+                            scrollbar: false,
+                            area: ['450px', '270px'],
+                            skin: "layui-layer-molv",
+                            content: $('#popup_one')
+                        });
+                    });
+                    $('.delete-icon').click(function () {
+                        var user_id = $(this).attr('data-id');
+                        layer.confirm('确定要删除？', {
+                            skin: 'layui-layer-molv',
+                            btn: ['确认', '取消']
+                        }, function () {
+                            var url = '/root/management/' + user_id;
+                            $.ajax({
+                                type: 'delete',
+                                url: url,
+                                dataType: 'json',
+                                success: function (res) {
+                                    console.log(res);
+                                    if (res.status == 100000) {
+                                        layer.msg('删除成功', {
+                                            time: 1000,
+                                        });
+                                        setTimeout(function () {
+                                            tableIns.reload();
+                                        }, 1000)
+                                    }
+                                },
+                                error: function () {
+
+                                }
+                            });
+
+                        }, function () {
+
+                        });
+                    })
+                },
+                page: {
+                    layout: ['count', 'prev', 'page', 'next', 'skip'] //自定义分页布局
+                    , groups: 1 //只显示 1 个连续页码
+                    , first: false //不显示首页
+                    , last: false //不显示尾页
+                },
+                id: 'dataTable'
+            })
+        })
+    },
+    addUser: function () {
+        var _that = this;
+        $('#city_picker_search').address({
+            level: 2,
+            offsetLeft: 0
+        });
+        $('#add_user').click(function () {
+            layer.open({
+                type: 1,
+                title: '新增城市经理',
+                closeBtn: 1,
+                shadeClose: true,
+                scrollbar: false,
+                area: ['450px', '270px'],
+                skin: "layui-layer-molv",
+                content: $('#popup')
+            });
+        })
+    },
+    edit: function () {
+        var _that = this;
+    }
+};
+set.init();
+set.addUser();
+set.edit()
