8646d29ca832ef282b0565a5e5cf1b75146b04f8
wangpengfei <wangpf@huitouche.com>
2018-08-10 11:48:23 +0800

    用户管理

--- server/static/js/root/root.js
+++ server/static/js/root/root.js
@@ -3,11 +3,12 @@ var set = {
         var secondMenu = document.getElementById('second_menu_list');
         secondMenu.style.display = 'block';
         $('#second_menu_list>li:nth-of-type(2) a').addClass('selected-active');
-        $('#second_menu_box').addClass('menu-active')
+        $('#second_menu_box').addClass('menu-active');
         layui.use(['layer', 'table'], function () {
             var layer = layui.layer;
             var table = layui.table;
-            var url = '/root/management/'
+            var url = '/root/management/';
+
             var tableIns = table.render({
                 elem: '#root_table',
                 even: true,
@@ -23,7 +24,7 @@ var set = {
                     {field: 'region_name', title: '所属城市'},
                     {
                         field: 'region_id', title: '操作', width: 120, templet: function (d) {
-                            return '<i data-number="' + d.account + '" class="layui-icon layui-icon-edit edit-icon" title="编辑"  style="margin-right: 40px;cursor:pointer;">&#xe642;</i><i data-id="' + d.id + '" style="cursor: pointer" class="layui-icon delete-icon"  title="删除" >&#xe640;</i>'
+                            return '<i data-id="' + d.id + '" class="layui-icon layui-icon-edit edit-icon" title="编辑"  style="margin-right: 40px;cursor:pointer;">&#xe642;</i><i data-id="' + d.id + '" style="cursor: pointer" class="layui-icon delete-icon"  title="删除" >&#xe640;</i>'
                         }
                     }
                 ]],
@@ -31,6 +32,8 @@ var set = {
                     layer.closeAll('loading');
                     $('.edit-icon').click(function () {
                         var content = $(this).parents('tr').children('td:eq(0)').find('.layui-table-cell').text();
+                        user_id = $(this).attr('data-id');
+                        phone = $(this).parents('tr').children('td:eq(1)').find('.layui-table-cell').text();
                         var location = $(this).parents('tr').children('td:eq(2)').find('.layui-table-cell').text();
                         $('#name_edit').val(content);
                         $('#city_picker_search_second').val(location);
@@ -39,26 +42,59 @@ var set = {
                             title: '编辑信息',
                             closeBtn: 1,
                             shadeClose: true,
-                            scrollbar: false,
                             area: ['450px', '270px'],
                             skin: "layui-layer-molv",
                             content: $('#popup_one')
                         });
                     });
                     $('#confirm_add').click(function () {
+                        var phone_number = $('#phone_number').val();
+                        var city_picker_search = $('#city_picker_search').attr('cityid');
+                        var user_name = $('#user_name').val();
+                        var add_user_password = $('#add_user_password').val();
+                        var url = '/root/management/';
+                        var data = {
+                            "account": phone_number,
+                            "user_name": user_name,
+                            "password": add_user_password,
+                            "region_id": city_picker_search
+                        };
+                        data = JSON.stringify(data);
+                        http.ajax.post_no_loading(true, false, url, data, http.ajax.CONTENT_TYPE_2, function (res) {
+                            if (res.status == 100000) {
+                                layer.msg('添加成功', {
+                                    time: 700
+                                })
+                            }
+                            setTimeout(function () {
+                                layer.closeAll();
+                                tableIns.reload();
+                            }, 700)
+                        })
+                    });
+                    $('#confirm_fix').click(function () {
                         var name_edit = $('#name_edit').val();
                         var password = $('#password').val();
-                        var city_picker_search_second = $('#city_picker_search_second').val();
-                        var phone = $(this).attr('data-number');
+                        var city_picker_search_second = $('#city_picker_search_second').attr('cityid');
+                        console.log(user_id)
                         var data = {
                             "account": phone,
                             "user_name": name_edit,
                             "password": password,
                             "region_id": city_picker_search_second
                         };
-                        var url = '/root/management/';
-                        http.ajax.post_no_loading(true, false, url,data,http.ajax.CONTENT_TYPE_2,function(res){
-                            console.log(res)
+                        data = JSON.stringify(data);
+                        var url = '/root/management/' + user_id;
+                        http.ajax.put_no_loading(true, false, url, data, http.ajax.CONTENT_TYPE_2, function (res) {
+                            if (res.status == 100000) {
+                                layer.msg('修改成功', {
+                                    time: 700
+                                });
+                                setTimeout(function () {
+                                    layer.closeAll(); //
+                                    tableIns.reload()
+                                }, 700)
+                            }
                         })
                     });
                     $('.delete-icon').click(function () {
@@ -76,11 +112,11 @@ var set = {
                                     console.log(res);
                                     if (res.status == 100000) {
                                         layer.msg('删除成功', {
-                                            time: 1000,
+                                            time: 700,
                                         });
                                         setTimeout(function () {
                                             tableIns.reload();
-                                        }, 1000)
+                                        }, 700)
                                     }
                                 },
                                 error: function () {
@@ -109,17 +145,21 @@ var set = {
             level: 2,
             offsetLeft: 0
         });
+        $('#city_picker_search_second').address({
+            level: 2,
+            offsetLeft: 0
+        });
         $('#add_user').click(function () {
             layer.open({
                 type: 1,
                 title: '新增城市经理',
                 closeBtn: 1,
                 shadeClose: true,
-                scrollbar: false,
-                area: ['450px', '270px'],
+                area: ['450px', '320px'],
                 skin: "layui-layer-molv",
                 content: $('#popup')
             });
+
         })
     },
     edit: function () {
