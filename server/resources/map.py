50e07cb87e2cadaf92d995deca7ee3b090d38945
wangpengfei <wangpf@huitouche.com>
2018-09-17 19:05:46 +0800

    gengxin

--- server/resources/map.py
+++ server/resources/map.py
@@ -3,49 +3,21 @@ from flask_restplus import Resource
 from server import verify, operations, filters, api
 from server.meta.decorators import Response
 from server.utils.request import get_all_arg
-import server.document.map as doc
+import server.document.heat_map as doc
 
 
-class DistributionMap(Resource):
+class HeatMap(Resource):
 
     @staticmethod
-    @doc.distribution_map_param
-    @filters.DistributionMap.get_result(params=dict, data=dict)
-    @operations.DistributionMap.get_data(params=dict)
-    @verify.DistributionMap.check_params(params=dict)
+    @doc.heat_map_param
+    @filters.HeatMap.get_result(params=dict, data=dict)
+    @operations.HeatMap.get_data(params=dict)
+    @verify.HeatMap.check_params(params=dict)
     def get():
         resp = Response(params=get_all_arg())
 
         return resp
 
 
-class GoodsMap(Resource):
-
-    @staticmethod
-    @doc.goods_map_param
-    @filters.GoodsMap.get_result(params=dict, data=dict)
-    @operations.GoodsMap.get_data(params=dict)
-    @verify.GoodsMap.check_params(params=dict)
-    def get():
-        resp = Response(params=get_all_arg())
-
-        return resp
-
-
-class UsersMap(Resource):
-
-    @staticmethod
-    @doc.users_map_param
-    @filters.UsersMap.get_result(params=dict, data=dict)
-    @operations.UsersMap.get_data(params=dict)
-    @verify.UsersMap.check_params(params=dict)
-    def get():
-        resp = Response(params=get_all_arg())
-
-        return resp
-
-
-ns = api.namespace('map', description='地图工具')
-ns.add_resource(DistributionMap, '/distribution_map/')
-ns.add_resource(GoodsMap, '/goods_map/')
-ns.add_resource(UsersMap, '/users_map/')
+ns = api.namespace('map', description='热点图')
+ns.add_resource(HeatMap, '/heat_map/')
