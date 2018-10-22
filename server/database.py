948171874328e85bd92fed54eb24c8249285f50a
leo_he <hexm@huitouche.com>
2018-10-22 17:35:05 +0800

    完善缓存装饰器

--- server/database.py
+++ server/database.py
@@ -1,12 +1,11 @@
 # coding=utf-8
 # author=veficos
-import redis
+from werkzeug.contrib.cache import RedisCache
 
-from server.meta.creators import DictModel
 from server.configs import configs
-from server.mysqldb import MySQLdb
-from server.mysqldb import MongoLinks
+from server.meta.creators import DictModel
 from server.mysqldb import ExtendRedis
+from server.mysqldb import MySQLdb
 
 db = DictModel({
     'read_db': MySQLdb(dict(configs.remote.union.mysql.read2_db.get())),
@@ -21,10 +20,10 @@ db = DictModel({
 
 pyredis = DictModel({
     'token': ExtendRedis(ip=configs.remote.union.redis.token.host,
-                          port=configs.remote.union.redis.token.port,
-                          db=configs.remote.union.redis.token.db),
-    'da_cacher': redis.StrictRedis(connection_pool=redis.ConnectionPool(host=configs.remote.union.redis.da_cacher.host,
-                                                                       port=configs.remote.union.redis.da_cacher.port,
-                                                                       db=configs.remote.union.redis.da_cacher.db)),
+                         port=configs.remote.union.redis.token.port,
+                         db=configs.remote.union.redis.token.db),
 })
 
+redis_cache_conn = RedisCache(host=configs.remote.union.redis.da_cacher.host,
+                              port=configs.remote.union.redis.da_cacher.port,
+                              db=configs.remote.union.redis.da_cacher.db)
--- server/meta/redis_cache.py
+++ server/meta/redis_cache.py
@@ -1,7 +1,7 @@
 import functools
 import json
 
-from server.database import pyredis
+from server.database import redis_cache_conn
 from server.status import make_resp, APIStatus, HTTPStatus
 from server.utils.gen_fp import gen_fp
 
@@ -11,14 +11,14 @@ def redis_cache(expire_time=1800):
         @functools.wraps(func)
         def wrapped_func(*args, **kwargs):
             fp = gen_fp()
-            result = pyredis.da_cacher.get(fp)
+            result = redis_cache_conn.get(fp)
             if result:
                 print("=" * 50, "Using Cache")
                 result = json.loads(result)
                 return make_resp(APIStatus.Ok, count=result[0].get("count", 0), data=result[0].get("data", [])), HTTPStatus.Ok
             result = func(*args, **kwargs)
             print("=" * 50, "Not Using Cache")
-            pyredis.da_cacher.setex(fp, expire_time, json.dumps(result))
+            redis_cache_conn.set(fp, json.dumps(result), expire_time)
             return result
         return wrapped_func
     return decorator
